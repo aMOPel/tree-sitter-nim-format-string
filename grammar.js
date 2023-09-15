d678bca42c8b424a57938cfca1d9ad787ff68a2f
aMOPel <momoPFL@gmail.com>
2023-09-15 22:32:31 +0200

    added refined grammar and added format_specifiers and tests

--- grammar.js
+++ grammar.js
@@ -7,12 +7,45 @@ module.exports = grammar({
 
   rules: {
     source_file: $ => optional($.string_content),
-    string_content: $ => repeat1(choice($.matching_curlies, $._everything_else)),
-    _everything_else: $ => /[^\n\r\s]/,
+    string_content: $ => repeat1(choice($.matching_curlies, $._everything_else, $.double_curlies)),
     matching_curlies: $ => seq(
-      '{',
-      alias(repeat($._everything_else), $.everything_else_inside),
-      '}',
+      field('opening_curly', $.opening_curly),
+      field('nim_expression', alias(repeat1(choice(
+        /[^:={}\n\r\s]/,
+        field('escaped_curly', $.escaped_curly,),
+      )), $.nim_expression)),
+      optional(field('equals', $.equals)),
+      optional($.format_specifiers),
+      field('closing_curly', $.closing_curly),
     ),
+
+    format_specifiers: $ => seq(
+      $.colon,
+      optional(field('fill_align', $.fill_align)),
+      optional(field('sign', $.sign)),
+      optional(field('hash', $.hash)),
+      optional(field('zero', $.zero)),
+      optional(field('min_width', $.min_width)),
+      optional(field('precision', $.precision)),
+      optional(field('type', $.type)),
+    ),
+
+    _everything_else: _ => /[^{}\n\r\s]/,
+    double_curlies: _ => choice('{{', '}}'),
+    escaped_curly: _ => choice('\\{', '\\}'),
+    opening_curly: _ => '{',
+    closing_curly: _ => '}',
+    equals: _ => '=',
+
+    colon: _ => token.immediate(':'),
+    // NOTE: `[^{}<\^>]` is not according to spec but otherwise things break
+    // BUG: the fill char can't be <space>, collision with $.sign
+    fill_align: _ => token.immediate(/[^{}<\^>\n\r]?[<\^>]/),
+    sign: _ => token.immediate(/[\-+ ]/),
+    hash: _ => token.immediate('#'),
+    zero: _ => token.immediate('0'),
+    min_width: _ => token.immediate(/[1-9][0-9]*/),
+    precision: _ => token.immediate(/\.[1-9][0-9]*/),
+    type: _ => token.immediate(/[bdoxXeEfFgG]/),
   }
 });
